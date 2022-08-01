1e881bff1c02b097b7cedc25d76ad7431f44b1e6
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-08-01 21:55:46 +1000

    setup form component

---
+++ src/Components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [location, setLocation] = useState("");
+
+  return (
+    <form>
+      <input type="text" />
+    </form>
+  );
+}
+
+export default App;
