47e48d2b03160691d7449bd5d8f5d08feb4d9f3e
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-08-14 21:45:54 +1000

    installed tailwind for styling

--- src/Components/Weather component/Weather.tsx
+++ src/Components/Weather component/Weather.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export function Weather() {
   return (
     <div>
-      <p>Temperature</p>
+      <p className="text-3xl font-bold underline">Monday</p>
+      <p>Temperature</p> <p>Humidity</p>
+      <p>Cloudy</p>
     </div>
   );
 }
