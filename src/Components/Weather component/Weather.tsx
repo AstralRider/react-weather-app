712527db35a5d13dac0bf13faeacc85cd7b2f59e
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-08-19 17:46:58 +1000

    weather component styling added

--- src/Components/Weather component/Weather.tsx
+++ src/Components/Weather component/Weather.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Weather() {
   return (
-    <div className="flex justify-center">
+    <div className="container flex justify-center max-w-2xl mx-auto">
       <div>
         <p className="">Monday</p>
         <p className="">Temperature</p> <p className="">Humidity</p>
