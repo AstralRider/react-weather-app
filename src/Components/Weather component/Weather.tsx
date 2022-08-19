883b222eaf8cbfa5b852be0914910f7d8b0ada7f
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-08-19 20:31:06 +1000

    further weather card styling

--- src/Components/Weather component/Weather.tsx
+++ src/Components/Weather component/Weather.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export function Weather() {
   return (
-    <div className="container flex justify-center max-w-2xl mx-auto">
-      <div>
+    <div className="container flex justify-center max-w-2xl mx-auto ">
+      <div className="rounded shadow-2xl outline outline-1 outline-gray-200 px-5 py-6">
         <p className="">Monday</p>
         <p className="">Temperature</p> <p className="">Humidity</p>
         <p className="">Cloudy</p>
