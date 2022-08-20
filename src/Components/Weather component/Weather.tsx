b687350e7f89705ded89cc5849c398725869d298
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-08-20 18:38:48 +1000

    added hover effect to card

--- src/Components/Weather component/Weather.tsx
+++ src/Components/Weather component/Weather.tsx
@@ -3,13 +3,13 @@ import React from "react";
 export function Weather() {
   return (
     <div className="container flex justify-center max-w-2xl mx-auto ">
-      <div className="flex flex-col rounded shadow-xl outline outline-1 outline-gray-200 px-8 py-4">
+      <div className="flex flex-col rounded shadow-xl outline outline-1 outline-gray-200 px-8 py-4 transition ease-in-out delay-10  hover:-translate-y-1 hover:scale-110">
         <div className=" w-16 h-16 flex self-center">
           <img className="" src="/Assets/clouds.png" alt="" />
         </div>
         <div className="">
           <p className="text-gray-500">Monday</p>
-          <p className="">Temp:</p>
+          <p className="text-gray-500">Temp:</p>
         </div>
       </div>
     </div>
