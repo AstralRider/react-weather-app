374ce2bdaa9b8bb4fff4e88fb48b28ec7a1d6d9d
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-08-20 18:25:32 +1000

    weather card styling

--- src/Components/Weather component/Weather.tsx
+++ src/Components/Weather component/Weather.tsx
@@ -3,10 +3,14 @@ import React from "react";
 export function Weather() {
   return (
     <div className="container flex justify-center max-w-2xl mx-auto ">
-      <div className="rounded shadow-2xl outline outline-1 outline-gray-200 px-5 py-6">
-        <p className="">Monday</p>
-        <p className="">Temperature</p> <p className="">Humidity</p>
-        <p className="">Cloudy</p>
+      <div className="flex flex-col rounded shadow-xl outline outline-1 outline-gray-200 px-8 py-4">
+        <div className=" w-16 h-16 flex self-center">
+          <img className="" src="/Assets/clouds.png" alt="" />
+        </div>
+        <div className="">
+          <p className="text-gray-500">Monday</p>
+          <p className="">Temp:</p>
+        </div>
       </div>
     </div>
   );
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,7 @@
 const getData = async () => {
   try {
     const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/forecast?q=Tokyo&appid=6078affb6cb911d495ce820cdc4b8eeb&units=metric"
+      "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=6078affb6cb911d495ce820cdc4b8eeb&units=metric"
     );
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
