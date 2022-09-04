0b58aa35223266fe602e6e59d5c49cb8d1b68ecb
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-09-04 12:14:41 +1000

    some css changes to searchbar

--- src/Components/Form Component/SearchBar.tsx
+++ src/Components/Form Component/SearchBar.tsx
@@ -26,10 +26,11 @@ export function SearchBar({ setLocation }: Props) {
         className="container max-h-40 flex justify-center "
       >
         <input
-          className="outline outline-2 outline-gray-300 rounded-md w-80 h-10 min-w-min text-3xl"
+          className="outline outline-2 outline-gray-300 rounded-md w-80 h-10 min-w-min text-3xl px-2 text-gray-500"
           type="text"
           onChange={handleChange}
           value={search}
+          placeholder="Enter a city"
         />
       </form>
     </div>
